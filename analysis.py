5c6db79e0044e5d9875a52f7fb9b0fb8461c171b
Michael Scheer <mgscheer@gmail.com>
2016-06-24 09:55:00 +0200

    work at QuTech

--- analysis.py
+++ analysis.py
@@ -138,27 +138,27 @@ def spin_fit_fun_single(N, phi, x):
 # equivalent to np.array([single_spin_fit_fun(N, phi, x) for phi, x in zip(phis, xs)]).prod()
 def spin_fit_fun(N, *args):
 	num_spins = len(args)/2
-	phis, xs = args[:num_spins], args[num_spins:]
+	phis, xs = args[:num_spins], args[num_spins:2*num_spins]
 	return reduce(lambda accum, next: accum * spin_fit_fun_single(N, next[0], next[1]), zip(phis, xs), 1.0)
 
 # for two arrays l1 and l2 returns the squared error between them
 def squared_error(l1, l2):
 	return sum((l1 - l2) ** 2)
 
-def spin_fit_guess(N_vals, N_data, num_spins, param_guess, error_fun = squared_error):
+def spin_fit_guess(N_vals, N_data, num_spins, param_guess, error_fun = squared_error, fit_fun = spin_fit_fun):
 	try:
 		with warnings.catch_warnings():
 			warnings.simplefilter("ignore")
-			popts, pcovs = opt.curve_fit(spin_fit_fun, N_vals, N_data, p0 = param_guess)
-		xs = popts[num_spins:]
-		error = error_fun(spin_fit_fun(N_vals, *popts), N_data)
+			popts, pcovs = opt.curve_fit(fit_fun, N_vals, N_data, p0 = param_guess)
+		xs = popts[num_spins:2*num_spins]
+		error = error_fun(fit_fun(N_vals, *popts), N_data)
 		return popts, error, np.all((xs > 0) & (xs <= 2))
 	except (RuntimeError, TypeError):
 		raise FitError("Failed to fit spins with N data.")
 
 # fit possibly several spins using a spin_fit_fun to N_data
 # for reference, see http://stackoverflow.com/questions/13405053/scipy-leastsq-fit-to-a-sine-wave-failing
-def spin_fit(N_vals, N_data, error_fun = squared_error, verbose = True, plots = True):
+def spin_fit(N_vals, N_data, error_fun = squared_error, verbose = True, plots = True, fit_fun = spin_fit_fun, extra_params = []):
 	coefs, freqs = sinusoid_coefs_fft(N_vals, N_data)
 	likely_freqs = find_peaks_from_fourier(coefs, freqs)
 	params, errors, spins_found = [], [], []
@@ -173,9 +173,9 @@ def spin_fit(N_vals, N_data, error_fun = squared_error, verbose = True, plots = 
 	for num_spins in range(1,1 + highest_spin(len(likely_freqs))):
 		approx_phis = gen_freqs_from_fourier(likely_freqs, num_spins)
 		approx_xs = num_spins * [2.0]
-		p0s = approx_phis + approx_xs
+		p0s = approx_phis + approx_xs + extra_params[:num_spins]
 		try:
-			popts, error, valid = spin_fit_guess(N_vals, N_data, num_spins, p0s, error_fun = error_fun)
+			popts, error, valid = spin_fit_guess(N_vals, N_data, num_spins, p0s, error_fun = error_fun, fit_fun = fit_fun)
 			if valid:
 				params.append(popts)
 				errors.append(error)
@@ -197,7 +197,7 @@ def spin_fit(N_vals, N_data, error_fun = squared_error, verbose = True, plots = 
 			plt.show()
 			plt.figure(figsize=(10,10))
 			plt.plot(N_vals, N_data, '.-k', lw=0.4, label = 'data')
-			plt.plot(N_vals, spin_fit_fun(N_vals, *params[ind]), label = 'fit')
+			plt.plot(N_vals, fit_fun(N_vals, *params[ind]), label = 'fit')
 			plt.title('Fitted N data')
 			plt.xlabel('N')
 			plt.ylabel('M')
@@ -205,53 +205,60 @@ def spin_fit(N_vals, N_data, error_fun = squared_error, verbose = True, plots = 
 			plt.show()
 		return params[ind], spins_found[ind]
 
-def repeated_spin_fit(N_vals, N_data, error_fun = squared_error, num_subsets = 4, verbose = True, plots = True):
+def repeated_spin_fit(N_vals, N_data, error_fun = squared_error, num_subsets = 4,
+	fit_fun = spin_fit_fun, extra_params = [], verbose = True, plots = True):
 	spin_fits, scaled_errors = [], []
 	for subset in [np.arange(int(len(N_vals) * r)) for r in np.linspace(0, 1, 1 + num_subsets)[1:]]:
 		try:
-			spin_ans = spin_fit(N_vals[subset], N_data[subset], error_fun = error_fun, verbose = verbose, plots = plots)
+			spin_ans = spin_fit(N_vals[subset], N_data[subset], error_fun = error_fun, fit_fun = fit_fun,
+				extra_params = extra_params, verbose = verbose, plots = plots)
 			spin_fits.append(spin_ans)
-			scaled_errors.append(1.0 * error_fun(spin_fit_fun(N_vals, *spin_ans[0]), N_data)/len(N_vals))
+			scaled_errors.append(1.0 * error_fun(fit_fun(N_vals, *spin_ans[0]), N_data)/len(N_vals))
 		except FitError:
 			pass
 	if not spin_fits:
 		raise FitError("Failed to fit spins with N data.")
 	min_ind = np.argmin(scaled_errors)
 	popt_spin, num_spins = spin_fits[min_ind]
-	if min_ind != len(scaled_errors) - 1:
-		try:
-			popts, error, valid = spin_fit_guess(N_vals, N_data, num_spins, popt_spin, error_fun = error_fun)
-			if valid:
-				spin_fits.append((popts, num_spins))
-				scaled_errors.append(1.0 * error_fun(spin_fit_fun(N_vals, *popts), N_data)/len(N_vals))
-				min_ind = np.argmin(scaled_errors)
-				popt_spin, num_spins = spin_fits[min_ind]
-			if plots:
-				plt.figure(figsize = (10,10))
-				plt.plot(N_vals, N_data, '.-k', lw=0.4, label = 'data')
-				plt.plot(N_vals, spin_fit_fun(N_vals, *popts), label = 'fit')
-				plt.title('Fitted N data')
-				plt.xlabel('N')
-				plt.ylabel('M')
-				plt.legend()
-				plt.show()
-		except FitError:
-			pass
 	if verbose:
-		print "best: ", min_ind
+		print "best before final pass: ", min_ind
+		phis, xs = np.array(popt_spin[:num_spins]) % np.pi, np.array(popt_spin[num_spins:2*num_spins])
+		phis, xs = map(np.array, zip(*sorted(zip(phis, xs), key = lambda x:-x[1])))# sort phis in decreasing order by xs
+		print "answer before final pass: ", xs, phis
+	try:
+		popts, error, valid = spin_fit_guess(N_vals, N_data, num_spins, popt_spin, error_fun = error_fun, fit_fun = fit_fun)
+		if valid:
+			spin_fits.append((popts, num_spins))
+			scaled_errors.append(1.0 * error_fun(fit_fun(N_vals, *popts), N_data)/len(N_vals))
+			min_ind = np.argmin(scaled_errors)
+			popt_spin, num_spins = spin_fits[min_ind]
+		if plots:
+			plt.figure(figsize = (10,10))
+			plt.plot(N_vals, N_data, '.-k', lw=0.4, label = 'data')
+			plt.plot(N_vals, fit_fun(N_vals, *popts), label = 'fit')
+			plt.title('Fitted N data')
+			plt.xlabel('N')
+			plt.ylabel('M')
+			plt.legend()
+			plt.show()
+	except FitError:
+		pass
+	if verbose:
+		print "best overall: ", min_ind
+		print scaled_errors
 	scaled_error = scaled_errors[min_ind]
-	phis, xs = np.array(popt_spin[:num_spins]) % np.pi, np.array(popt_spin[num_spins:])
-	phis, xs = map(np.array, zip(*sorted(zip(phis, xs), key = lambda x:-x[1])))# sort phis in decreasing order by xs
+	phis, xs = np.array(popt_spin[:num_spins]) % np.pi, np.array(popt_spin[num_spins:2*num_spins])
+	#phis, xs = map(np.array, zip(*sorted(zip(phis, xs), key = lambda x:-x[1])))# sort phis in decreasing order by xs
 	return phis, xs, scaled_error    
 
 # creates a tau vector with parameters that are suitable for the N value
-def choose_tau_params(N):
+def choose_tau_params(N, num_samples = 5100):
 	if N != 64:
 		raise FitError("N should be 64")
 	else:
 		min_tau = 3.0000000000000001e-06
 		max_tau = 2.1999999999999999e-05
-		length = 5100
+		length = num_samples
 		tau = np.linspace(min_tau, max_tau, length)
 		return tau
 
@@ -259,18 +266,19 @@ def choose_tau_params(N):
 # it finds all relative minima below fit_dips_below and keeps only the num_dips widest of them
 # returns the dip indices and the windows that define each dip, sorted by a measure of quality that accounts for both
 # isolation and depth of dip
-def find_resonances(data, fit_dips_below = None):
+def find_resonances(data, fit_dips_below = None, max_width = None):
 	min_inds = (signal.argrelextrema(data, np.less)[0]).astype(int)
 	max_inds = (signal.argrelextrema(data, np.greater)[0]).astype(int)
 	widths = max_inds[1:] - max_inds[:-1]
 	dip_inds = min_inds[1:] if min_inds[0] < max_inds[0] else min_inds[:-1]
 	windows = [(max_inds[i], max_inds[i+1]) for i in range(len(dip_inds)-1)] # should this be 3?
-	# a dip must go below fit_dips_below, unless fit_dips_below = None
-	filter_fun = lambda p: ((data[p[0]] < fit_dips_below) if (fit_dips_below != None) else True)
+	# a dip must go below fit_dips_below, unless fit_dips_below = None and it's width should be at most max_width unless max_width = None
+	filter_fun = lambda p: ((data[p[0]] < fit_dips_below) if (fit_dips_below != None) else True) and (p[1] <= max_width if max_width != None else True)
 	dip_inds, widths, windows = map(np.array, zip(*filter(filter_fun, zip(dip_inds, widths, windows))))
 	# sort by width
-	sort_inds = np.argsort(-1 * widths)
-	return dip_inds[sort_inds], windows[sort_inds]
+	# sort_inds = np.argsort(-1 * widths)
+	# return dip_inds[sort_inds], windows[sort_inds]
+	return dip_inds, windows
 
 # Calculates all possible omega_tilde values for a given phi and x at a given res_tau
 def calc_omega_tilde(phi, x, res_tau, omega_larmor):
@@ -297,11 +305,12 @@ def calc_omega_tilde(phi, x, res_tau, omega_larmor):
 # it adds that spin to the spin_dict which was passed in. It returns the spin_dict and a boolean indicating if the fit
 # was successful or not.
 def analyze_dip(dip_ind, tau, data_func, omega_larmor, spin_dict = {}, error_tol = .1/64,
-				N_vals = np.arange(0,256,2), error_fun = squared_error, num_subsets = 4, verbose = False, plots = False):
+				N_vals = np.arange(0,256,2), error_fun = squared_error, fit_fun = spin_fit_fun, extra_params = [],
+				num_subsets = 4, verbose = False, plots = False):
 	res_tau = tau[dip_ind]
 	N_data = data_func(N_vals, res_tau)
 	try:
-		phis, xs, scaled_error = repeated_spin_fit(N_vals, N_data, error_fun = error_fun,
+		phis, xs, scaled_error = repeated_spin_fit(N_vals, N_data, error_fun = error_fun, fit_fun = fit_fun, extra_params = extra_params,
 			num_subsets = num_subsets, verbose = verbose, plots = plots)
 	except FitError:
 		if verbose:
@@ -311,6 +320,7 @@ def analyze_dip(dip_ind, tau, data_func, omega_larmor, spin_dict = {}, error_tol
 		print "res_tau: ", res_tau
 		print "scaled_error <= error_tol: ", scaled_error <= error_tol
 		print "scaled_error: ", scaled_error
+		phis, xs = map(np.array, zip(*sorted(zip(phis, xs), key = lambda x:-x[1])))# sort phis in decreasing order by xs
 		print "xs, phis, scaled_error: ", xs, phis, scaled_error
 	if scaled_error > error_tol:
 		return spin_dict
@@ -418,6 +428,7 @@ def analyze_diamond(data_func, N, omega_larmor, verbose = False, plots = False):
 	tau = choose_tau_params(N)
 	data = data_func(N, tau)
 	dip_inds, windows = find_resonances(data, fit_dips_below = None)
+	print sum(dip_inds >= 3220)
 	spin_dict = {}
 	for dii in range(len(dip_inds)):
 		dip_ind = dip_inds[dii]
@@ -434,122 +445,3 @@ def analyze_diamond(data_func, N, omega_larmor, verbose = False, plots = False):
 	while num_remove <= 2:
 		As, Bs, num_remove = remove_spins(As, Bs, N, omega_larmor, tau, data, num_remove = num_remove, error_fun = squared_error, verbose=verbose)
 	return As, Bs, all_guess_As, all_guess_Bs, select_As, select_Bs, guess_As, guess_Bs, cluster_As, cluster_Bs
-
-"""
-
-# calculates the right side of equation 10 of the supplement of Taminiau et al 2012 minus the left side as a function of omega (omega_tilde).
-# anywhere where mz is more than 1 in magnitude, the result is -1-x where x is the left side.
-# it also returns whether or not the calculated curve goes through 0
-def omega_root_fun(omega, res_tau, cosphi, x, omega_larmor):
-	omega = np.array(omega)
-	beta = omega_larmor * res_tau
-	alpha = omega * res_tau
-	mz = (np.cos(alpha) * np.cos(beta) - cosphi)/(np.sin(alpha) * np.sin(beta))
-	x_approx = (1 - mz ** 2) * (1-np.cos(alpha)) * (1-np.cos(beta))/(1+cosphi)
-	bad_inds = np.where(mz ** 2 > 1)[0]
-	x_approx[bad_inds] = -1
-	return x_approx - x, np.all(x_approx < x) or np.all(x_approx > x)
-
-
-# fits the N data at a particular tau with spins (trying several subsets of N_vals and taking whichever fits best)
-# finds the correct value for cosphi (finds its sign correctly) and also the omege root array for that value of cosphi
-# returns the xs along with their cosphis and root arrays as well as the error level that was achieved in the spin fit
-def analyze_tau(tau_fixed, N_data, omega_larmor, omega_array, N_vals, plots = False):
-	spin_fits = []
-	scaled_errors = []
-	for subset in [np.arange(len(N_vals)/2), np.arange(len(N_vals) * 3/4), np.arange(len(N_vals))]: # different N ranges
-		try:
-			spin_ans = spin_fit(N_vals[subset], N_data[subset], verbose = False, plots = plots)
-			spin_fits.append(spin_ans)
-			scaled_errors.append(1.0 * spin_ans[1]/len(subset))
-		except FitError:
-			pass
-	if not spin_fits:
-		raise FitError("Failed to fit spin with N data.")
-	popt_spin, spin_error, num_spins = spin_fits[np.argmin(scaled_errors)]
-	xs = popt_spin[num_spins:]
-	phis, xs = map(np.array, zip(*sorted(zip(popt_spin[:num_spins], xs), key = lambda x:-x[1])))# sort phis in decreasing order by xs
-	cosphis = np.cos(phis) # could be positive or negative, if this is wrong it gets corrected below
-	corrected_cosphis, root_arrays = [], []
-	for x, cosphi in zip(xs, cosphis):
-		correct_cosphi = cosphi
-		root_array, wrong_sign = omega_root_fun(omega_array, tau_fixed, correct_cosphi, x, omega_larmor)
-		if wrong_sign:
-			correct_cosphi = -cosphi
-			root_array, wrong_sign = omega_root_fun(omega_array, tau_fixed, correct_cosphi, x, omega_larmor)
-		root_arrays.append(root_array)
-		corrected_cosphis.append(correct_cosphi)
-	return xs, corrected_cosphis, root_arrays, min(scaled_errors)
-
-# for a given dip_ind, measures N data and fits it using analyze_tau. If the fit error is less than error_tol, it
-# fits spins_per_dip of the spins to an omega value and therefore to an A and B. If this A and B are within the bounds
-# it adds that spin to the spin_dict which was passed in. It returns the spin_dict and a boolean indicating if the fit
-# was successful or not.
-def analyze_dip(dip_ind, tau, data_func, omega_larmor, spin_dict = {}, error_tol = .1/64, omega_acc = .0001 * mag,
-				spins_per_dip = 2, N_vals = np.arange(0,256,2), verbose = False, plots = False):
-	lower_bound, upper_bound = omega_bounds(omega_larmor)
-	omega_array = np.linspace(lower_bound, upper_bound, (upper_bound - lower_bound)/omega_acc)
-	tau_fixed = tau[dip_ind]
-	N_data = data_func(N_vals, tau_fixed)
-	try:
-		xs, cosphis, root_arrays, scaled_error = analyze_tau(tau_fixed, N_data, omega_larmor, omega_array, N_vals = N_vals, plots = plots)
-		if verbose:
-			print "xs, cosphis, scaled_error: ", xs, cosphis, scaled_error
-			print "scaled_error > error_tol: ", scaled_error > error_tol
-		if scaled_error > error_tol:
-			return spin_dict, False
-		for x, cosphi, root_array in zip(xs, cosphis, root_arrays)[:spins_per_dip]: # find the roots of the root_array
-			sign_diffs = np.diff(np.sign(root_array))
-			zero_inds = np.concatenate((np.where(sign_diffs == 2.0)[0] + 1, np.where(sign_diffs == -2.0)[0]))
-			for omega in omega_array[zero_inds]: # all the possible omegas
-				A, B = calc_A_B(cosphi, omega, omega_larmor, tau_fixed)
-				if valid_A_B(A, B):
-					if (dip_ind, x, cosphi) in spin_dict:
-						spin_dict[(dip_ind, x, cosphi)] += [(A, B)]
-					else:
-						spin_dict[(dip_ind, x, cosphi)] = [(A, B)]
-		return spin_dict, True
-	except FitError:
-		return spin_dict, False
-
-
-
-# takes in a dict of the type returned by analyze_dip and for each entry which represents a particular spin found at a particular dip
-# it chooses the A, B value that minimizes the squared error from the tau data. It returns these A and B guesses along with the calculated errors.
-def choose_spin_guesses(spin_dict, N, omega_larmor, tau, data, x_min = 1.5, error_fun = squared_error):
-	guess_As, guess_Bs, dataerrs = [], [], []
-	for k in spin_dict.keys():
-		if k[2] >= x_min: # the x value
-			err, all_B_abs = [], []
-			for A, B in spin_dict[k]:
-				err.append(error_fun(calc_M_single(A, B, N, omega_larmor, tau), data))
-				all_B_abs.append(np.abs(B))
-			best_A, best_B = spin_dict[k][np.argmin(err)]
-			if best_B >= mag: # in case it's just choosing the one with smallest B since it is usually 1
-				guess_As.append(best_A)
-				guess_Bs.append(best_B)
-				dataerrs.append(min(err))
-	return np.array(guess_As), np.array(guess_Bs), np.array(dataerrs)
-
-
-def analyze_diamond(data_func, N, omega_larmor, verbose = False, plots = False):
-	tau = choose_tau_params(N)
-	data = data_func(N, tau)
-	dip_inds, windows = find_resonances(data, fit_dips_below = None)
-	dip_scaler, dip_clf = learning.load_obj("classifiers/dip_scaler_rbf"), learning.load_obj("classifiers/dip_clf_rbf")
-	def classify_dii(dii):
-		features = learning.dip_features(dii, dip_inds, data, tau, windows)
-		return dip_clf.predict(dip_scaler.transform([features]))
-	clf_dii = filter(classify_dii, range(len(dip_inds)))
-	spin_dict = {}
-	for dii in clf_dii:
-		dip_ind = dip_inds[dii]
-		spin_dict = analyze_dip(dip_ind, tau, data_func, omega_larmor, spin_dict, spins_per_dip = 2, N_vals = np.arange(0,256,2),
-			error_tol = .1/64, verbose = verbose, plots = plots)
-	guess_As, guess_Bs, dataerrs = choose_spin_guesses(spin_dict, N, omega_larmor, tau, data, x_min = 1.5, error_fun = squared_error)
-	cluster_As, cluster_Bs, cluster_dataerrs = cluster_spin_guesses(guess_As, guess_Bs, dataerrs, eps = .075, min_samples = 1)
-	As, Bs, num_remove = cluster_As, cluster_Bs, 1
-	while num_remove < 3:
-		As, Bs, num_remove = remove_spins(As, Bs, N, omega_larmor, tau, data, num_remove = num_remove, error_fun = squared_error)
-	return As, Bs
-"""
